c24c127d3fd860a754e3c0462766a51e3626fea7
DjebbZ <khalid.jebbari@gmail.com>
2015-12-31 15:39:26 +0100

    Rename unused variable

--- src/om_next_tutorial/core.cljs
+++ src/om_next_tutorial/core.cljs
@@ -14,7 +14,7 @@
       (dom/div nil
                (dom/span nil (str "Count: " count))
                (dom/button
-                 #js {:onClick (fn [e] (swap! app-state update-in [:count] inc))}
+                 #js {:onClick (fn [_] (swap! app-state update-in [:count] inc))}
                  "Click me")))))
 
 (def reconciler
